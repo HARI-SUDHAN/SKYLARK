5650fce451eec6a13f5af538583d373caea31f2b
HARI-SUDHAN <144309680+HARI-SUDHAN@users.noreply.github.com>
2025-12-01 22:52:40 +0530

    Add files via upload

---
+++ pictures/about.php
@@ -0,0 +1,38 @@
+<?php include("head.html")
+?>
+<body>
+<div class="about">
+<div class="img1">
+<marquee direction="up">
+<img src="image/logo.jpg" height="300px" width="300px" />
+</marquee>
+</div>
+<div class="img2">
+<marquee direction="down">
+<img src="image/logo.jpg" height="300px" width="400px" />
+</marquee>
+</div>
+<div class="img3">
+<marquee direction="left">
+<img src="image/logo.jpg" height="300px" width="300px" />
+</marquee>
+</div>
+<div class="img4">
+<marquee direction="right">
+<img src="image/logo.jpg" height="300px" width="300px" />
+</marquee>
+</div>
+<div class="img5">
+<marquee direction="up">
+<img src="image/logo.jpg" height="300px" width="400px" />
+</marquee>
+</div>
+<div class="img6">
+<marquee direction="down" scrollamount="20">
+<img src="image/logo.jpg" height="300px" width="300px" />
+</marquee>
+</div>
+</div>
+</body>
+<?php include("foot.html")
+?>
